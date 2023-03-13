e587d6592153b5b2bdda856fb3ababbb837860a0
NikhilM305 <nikhil.ninja007@gmail.com>
2023-03-13 23:55:31 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
 import Tesseract from 'tesseract.js';
-
+import './App.css'
 
 function App() {
 
   const [image, setImage] = useState(null);
-  const [text, setText] = useState(null);
+  const [text, setText] = useState([]);
   const [response, setResponse] = useState([]);
 
  
@@ -15,48 +15,18 @@ function App() {
   }
 
 
-  const handleImageToText = async () =>{
-//     Tesseract.recognize(image, "eng").then((res) => {
-//  setText(res.data.text)
-//     }).catch((err)=>console.log(err))
+  const handleImageToText = () =>{
+    Tesseract.recognize(image, "eng").then((res) => {
+ setText(res.data.text)
+    }).catch((err)=>console.log(err))
 
-    const find = await  Tesseract.recognize(image, "eng")
-    setText(find.data.text)
-    return find.data.text
-
-  }
-
-
-  const fetchQuestions = async (sum) => {
-    
-    
-      const options = {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-          'X-RapidAPI-Key': 'a8e75297c5msh0f90c48ec4ed7cep154f9cjsn736be15a10aa',
-          'X-RapidAPI-Host': 'chatgpt-ai-chat-bot.p.rapidapi.com'
-        },
-        body: JSON.stringify({ "query": `solve or summarize this ${sum}` })
-      };
-      // fetch('https://chatgpt-ai-chat-bot.p.rapidapi.com/ask', options)
-      //   .then(response => response.json())
-      //   .then(response => setResponse(prevResponse => [...prevResponse, response])) 
-      //   .catch(err => console.error(err));
-   const data = await  fetch('https://chatgpt-ai-chat-bot.p.rapidapi.com/ask', options)
-   const response = await data.json()
-   setResponse(response)
-
-
-  };
-
- 
-  const handleFunctions = async () =>{
- const sum = await handleImageToText()
-
-await fetchQuestions(sum)
+    // const find =  Tesseract.recognize(image, "eng")
+    // setText(find)
+   
 
   }
+
+
 
 
 console.log("text",text)
@@ -71,20 +41,20 @@ console.log("response",response)
 
   </div>
 <div>
-  <p>Select Image</p>
+ 
   <input type="file" onChange={handleChange} />
 </div>
 
 
 
 <button
- onClick={handleFunctions}
+ onClick={handleImageToText}
 >
   Convert Image to Text
 </button>
 
 
-<p>{response.response}</p>
+<p>{text}</p>
   </div>
 
 
