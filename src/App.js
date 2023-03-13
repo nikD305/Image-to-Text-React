967ff395596872b8b342dc81b4d1cdeb0b559041
NikhilM305 <nikhil.ninja007@gmail.com>
2023-03-14 00:04:39 +0530

    added loading

--- src/App.js
+++ src/App.js
@@ -5,61 +5,38 @@ import './App.css'
 function App() {
 
   const [image, setImage] = useState(null);
-  const [text, setText] = useState([]);
-  const [response, setResponse] = useState([]);
+  const [text, setText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
- 
-  const handleChange = (e) =>{
-  const image = e.target.files[0]
-  setImage(image)
+  const handleChange = (e) => {
+    const image = e.target.files[0]
+    setImage(image)
   }
 
-
-  const handleImageToText = () =>{
+  const handleImageToText = () => {
+    setIsLoading(true); // set loading state to true
     Tesseract.recognize(image, "eng").then((res) => {
- setText(res.data.text)
-    }).catch((err)=>console.log(err))
-
-    // const find =  Tesseract.recognize(image, "eng")
-    // setText(find)
-   
-
+      setText(res.data.text);
+      setIsLoading(false); // set loading state to false
+    }).catch((err) => {
+      console.log(err);
+      setIsLoading(false); // set loading state to false
+    });
   }
-
-
-
-
-console.log("text",text)
-console.log("response",response)
 
   return (
-
-
-  <div className="container">
-  <div>
-  <h2>Image to Text</h2>
-
-  </div>
-<div>
- 
-  <input type="file" onChange={handleChange} />
-</div>
-
-
-
-<button
- onClick={handleImageToText}
->
-  Convert Image to Text
-</button>
-
-
-<p>{text}</p>
-  </div>
-
-
-
-
+    <div className="container">
+      <div>
+        <h2>Image to Text</h2>
+      </div>
+      <div>
+        <input type="file" onChange={handleChange} />
+      </div>
+      <button onClick={handleImageToText}>
+        {isLoading ? 'Loading...' : 'Convert Image to Text'}
+      </button>
+      <p>{text}</p>
+    </div>
   );
 }
 
